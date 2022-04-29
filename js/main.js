29dcb0d243bdc4f2619f212775be3024a28e9ea0
Krystian Kopeć <73670385+Cannibal90@users.noreply.github.com>
2022-04-29 22:10:04 +0200

    add accorditions in faq

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const allNavLinks = document.querySelectorAll('.nav__link')
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
 const logo = document.querySelector('.brand')
+const accorditions = document.querySelectorAll('.faq__accordition-box')
 
 const onLinkChangeHandler = (navLink) => {
 	allNavLinks.forEach((link) => {
@@ -18,6 +19,14 @@ const onHamburgerClickHandler = () => {
 	navMobile.classList.toggle('nav-mobile--active')
 }
 
+const onChevronClickHandler = (accorditionText, chevronIcon, accordition) => {
+	accordition.classList.toggle('faq__accordition-box--active')
+	accorditionText.classList.toggle(
+		'faq__accordition-box-question-answer--active'
+	)
+	chevronIcon.classList.toggle('fa-chevron-up')
+}
+
 allNavLinks.forEach((link) => {
 	link.addEventListener('click', () => onLinkChangeHandler(link))
 })
@@ -27,3 +36,17 @@ logo.addEventListener('click', () => {
 })
 
 navBtn.addEventListener('click', onHamburgerClickHandler)
+
+accorditions.forEach((accordition) => {
+	const chevronButton = accordition.querySelector(
+		'.faq__accordition-box-toogle-button'
+	)
+	const accorditionText = accordition.querySelector(
+		'.faq__accordition-box-question-answer'
+	)
+	const chevronIcon = chevronButton.querySelector('.fa-solid')
+
+	chevronButton.addEventListener('click', () =>
+		onChevronClickHandler(accorditionText, chevronIcon, accordition)
+	)
+})
