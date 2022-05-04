5ef3264a26c0254fc56c6988540b8613e3c9c0a7
Krystian Kopeć <73670385+Cannibal90@users.noreply.github.com>
2022-05-05 00:54:22 +0200

    Add change bar collors and current year in footer

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
 const logo = document.querySelector('.brand')
 const accorditions = document.querySelectorAll('.faq__accordition-box')
+const allSections = document.querySelectorAll('.wrapper')
+const hamburgerBars = document.querySelector('.hamburger-inner')
+const footerYear = document.querySelector('.footer__year')
 
+//NAVIGATION
 const onLinkChangeHandler = (navLink) => {
 	allNavLinks.forEach((link) => {
 		link.classList.remove('nav__link--active')
@@ -19,6 +23,20 @@ const onHamburgerClickHandler = () => {
 	navMobile.classList.toggle('nav-mobile--active')
 }
 
+allNavLinks.forEach((link) => {
+	link.addEventListener('click', () => onLinkChangeHandler(link))
+})
+
+logo.addEventListener('click', () => {
+	allNavLinks.forEach((link) => {
+		link.classList.remove('nav__link--active')
+	})
+	window.scrollTo(0, 0)
+})
+
+navBtn.addEventListener('click', onHamburgerClickHandler)
+
+//ACCORDITIONS
 const onChevronClickHandler = (accorditionText, chevronIcon, accordition) => {
 	accordition.classList.toggle('faq__accordition-box--active')
 	accorditionText.classList.toggle(
@@ -26,16 +44,6 @@ const onChevronClickHandler = (accorditionText, chevronIcon, accordition) => {
 	)
 	chevronIcon.classList.toggle('fa-chevron-up')
 }
-
-allNavLinks.forEach((link) => {
-	link.addEventListener('click', () => onLinkChangeHandler(link))
-})
-
-logo.addEventListener('click', () => {
-	window.scrollTo(0, 0)
-})
-
-navBtn.addEventListener('click', onHamburgerClickHandler)
 
 accorditions.forEach((accordition) => {
 	const chevronButton = accordition.querySelector(
@@ -50,3 +58,32 @@ accorditions.forEach((accordition) => {
 		onChevronClickHandler(accorditionText, chevronIcon, accordition)
 	)
 })
+
+//CURRENT YEAR
+const handleYear = () => {
+	const currentDate = new Date()
+	footerYear.innerText = currentDate.getFullYear().toString()
+}
+handleYear()
+
+//SCROLL BARS
+const handleBarsObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach((section) => {
+		if (
+			(!section.classList.contains('white-section') &&
+				section.offsetTop <= currentSection + 60) ||
+			(section.classList.contains('reviews') &&
+				section.offsetTop <= currentSection + 60)
+		) {
+			hamburgerBars.classList.add('hamburger-white-bars')
+		} else if (
+			section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			hamburgerBars.classList.remove('hamburger-white-bars')
+		}
+	})
+}
+window.addEventListener('scroll', handleBarsObserver)
